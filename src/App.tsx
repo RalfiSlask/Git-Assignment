62ecb9e558d886081425ad51a9b03df5311bc7c3
RalfiSlask <nilssonnarki@gmail.com>
2024-03-26 08:19:00 +0100

    feat: create object with landinginfromation, integrated LandingIntro component with the object as prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import LandingIntro from './components/LandingIntro';
+
 function App() {
-  return <></>;
+  const landingInformation = {
+    title: 'Git Assignment',
+    description: 'This is an assignment that is testing my git knowledge',
+    quote: 'Be yourself, everyone else is already taken',
+  };
+
+  return (
+    <>
+      <LandingIntro landingInfo={landingInformation} />
+    </>
+  );
 }
 
 export default App;
