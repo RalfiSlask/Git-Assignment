18e709ace135f5cb3e750a1db6ec464da165e242
RalfiSlask <nilssonnarki@gmail.com>
2024-03-28 07:09:41 +0100

    feat: add hobby h2

--- src/components/Hobby.tsx
+++ src/components/Hobby.tsx
@@ -6,6 +6,12 @@ const Hobby: React.FC<{ hobby: string }> = ({ hobby }) => {
       <div>number</div>
     </>
   );
+  return (
+    <>
+      <h2>hobby</h2>
+      <div>{hobby}</div>
+    </>
+  );
 };
 
 export default Hobby;
