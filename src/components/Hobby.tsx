636abf0cf5442a4662b2fbd8bf2a8d30b613f50e
RalfiSlask <nilssonnarki@gmail.com>
2024-03-28 07:39:53 +0100

    feat: removed unneccesary code, add h3

--- src/components/Hobby.tsx
+++ src/components/Hobby.tsx
@@ -5,17 +5,6 @@ const Hobby: React.FC<{ hobby: string }> = ({ hobby }) => {
       <div>{hobby}</div>
       <div>lets rebase</div>
       <div>number</div>
-      <h3>number</h3>
-      <h4>4</h4>
-      <div>lets rebase</div>
-      <h5>number</h5>
-      <h4>7</h4>
-    </>
-  );
-  return (
-    <>
-      <h2>hobby</h2>
-      <div>{hobby}</div>
     </>
   );
 };
