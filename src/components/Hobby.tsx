b385209596743203719f849ae773dce05d254345
RalfiSlask <nilssonnarki@gmail.com>
2024-03-28 07:31:34 +0100

    feat: add a 4

--- src/components/Hobby.tsx
+++ src/components/Hobby.tsx
@@ -3,7 +3,8 @@ const Hobby: React.FC<{ hobby: string }> = ({ hobby }) => {
     <>
       <h4>Hobby number</h4>
       <div>{hobby}</div>
-      <div>number</div>
+      <h3>number</h3>
+      <h4>4</h4>
     </>
   );
 };
